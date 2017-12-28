f5198168a02ee85f896d999a13279f907a82ca77
TeodorDimitrov89 <braveheart1989@abv.bg>
2017-12-28 16:33:04 +0200

    learn how to navigate between pages.

--- ionic-2-basics/src/pages/users/user/user.ts
+++ ionic-2-basics/src/pages/users/user/user.ts
@@ -1,8 +1,19 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {NavController, NavParams} from "ionic-angular";
 @Component({
   selector: 'page-user',
-  template: 'user.html'
+  templateUrl: 'user.html'
 })
-export class UserPage {
+export class UserPage implements OnInit{
   name: string;
+  constructor(
+    private navParams: NavParams,
+    private navCtrl: NavController
+  ) {}
+  ngOnInit() {
+    this.name = this.navParams.get('username');
+  }
+  onGoBack() {
+    this.navCtrl.popToRoot(); //This method take us back to the bottom most page on the stack which is the root page.
+  }
 }
--- ionic-2-basics/src/pages/users/users.ts
+++ ionic-2-basics/src/pages/users/users.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {NavController} from "ionic-angular";
+import {UserPage} from "./user/user";
 // import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 // @IonicPage()
@@ -6,4 +8,10 @@ import { Component } from '@angular/core';
   selector: 'page-users',
   templateUrl: 'users.html',
 })
-export class UsersPage {}
+export class UsersPage {
+  constructor(private navCtrl: NavController) {}
+
+  onLoadUser(name:string) {
+    this.navCtrl.push(UserPage, {username: name});
+  }
+}
