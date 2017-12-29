258caa6432db7b91d132a073e85f5909f55104e1
TeodorDimitrov89 <braveheart1989@abv.bg>
2017-12-29 12:41:49 +0200

    learn about life cycle hooks

--- ionic-2-basics/src/pages/home/home.ts
+++ ionic-2-basics/src/pages/home/home.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import {UsersPage} from "../users/users";
-
+import {NavController} from "ionic-angular";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
   usrPage = UsersPage;
+
+  constructor(private navCtrl: NavController) {}
+
+  onGoToUsers() {
+    this.navCtrl.push(this.usrPage)
+      .catch((error) => console.log("Access denied! Argument was " + error))
+  }
 }
--- ionic-2-basics/src/pages/users/users.ts
+++ ionic-2-basics/src/pages/users/users.ts
@@ -14,4 +14,39 @@ export class UsersPage {
   onLoadUser(name:string) {
     this.navCtrl.push(UserPage, { username: name });
   }
+
+  ionViewCanEnter(): boolean | Promise<void> {
+    // console.log('ionViewCanEnter')
+    const rdm = Math.random();
+    return rdm > 0.5;
+  }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad');
+  }
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter');
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+  }
+
+
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave')
+  }
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave');
+  }
+  ionViewCanLeave(): boolean | Promise<void> {
+    console.log('ionViewCanLeave');
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, 1000)
+    })
+  }
+  ionViewWillUnload() {
+    console.log('ionViewWillUnload');
+  }
 }
