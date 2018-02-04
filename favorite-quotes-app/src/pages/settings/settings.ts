257a14a7de93245d9c89ab6d6fa1a9c8ce001769
TeodorDimitrov89 <braveheart1989@abv.bg>
2018-02-04 13:16:33 +0200

    Preparing the Settings Page

--- favorite-quotes-app/src/pages/settings/settings.ts
+++ favorite-quotes-app/src/pages/settings/settings.ts
@@ -1,18 +1,15 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Toggle} from 'ionic-angular';
 
-@IonicPage()
+
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html',
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingsPage');
+  onToggle(toggle: Toggle) {
+    console.log(toggle)
   }
 
 }
