713319ff8d2d8f900b37bd6d0607cbedbcc929ab
TeodorDimitrov89 <braveheart1989@abv.bg>
2018-02-04 11:17:26 +0200

    adding sidemenu

--- favorite-quotes-app/src/app/app.component.ts
+++ favorite-quotes-app/src/app/app.component.ts
@@ -3,12 +3,15 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import {TabsPage} from "../pages/tabs/tabs";
+import {SettingsPage} from "../pages/settings/settings";
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  tabsPage = TabsPage;
+  settingsPage = SettingsPage;
+
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +20,9 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+  onLoad(page: any) {
+
   }
 }
 
