1c7a6e095a54aa59cff05beaac58269cf4d9073d
TeodorDimitrov89 <braveheart1989@abv.bg>
2018-08-06 21:44:34 +0300

    configuring map, adding marker, passing data, displaying chosen location

--- Awesome-Places-App/src/pages/add-place/add-place.ts
+++ Awesome-Places-App/src/pages/add-place/add-place.ts
@@ -1,24 +1,41 @@
 import {Component} from '@angular/core';
-import { IonicPage, ModalController } from 'ionic-angular';
+import { ModalController } from 'ionic-angular';
 import {NgForm} from "@angular/forms";
 import {SetLocationPage} from "../set-location/set-location";
+import {Location} from "../../models/location";
 
-@IonicPage()
 @Component({
     selector: 'page-add-place',
     templateUrl: 'add-place.html',
 })
 export class AddPlacePage {
+    location: Location = {
+        lat: 42.682087,
+        lng: 23.324592
+    };
+    locationIsSet:boolean = false;
 
     constructor(private modalCtrl: ModalController) {}
 
     onSubmit(form: NgForm) {
         console.log(form.value)
     }
+
+
     onOpenMap() {
-        console.log('modal')
-        const modal = this.modalCtrl.create(SetLocationPage);
+        const modal = this.modalCtrl.create(SetLocationPage,
+            {
+                location: this.location,
+                isSet:this.locationIsSet
+            });
+
         modal.present();
+        modal.onDidDismiss(data => {
+            if(data) {
+                this.locationIsSet = true;
+                this.location = data.location;
+            }
+        });
     }
 
 }
--- Awesome-Places-App/src/pages/set-location/set-location.ts
+++ Awesome-Places-App/src/pages/set-location/set-location.ts
@@ -1,18 +1,35 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Location} from "../../models/location";
+import {NavParams, ViewController} from "ionic-angular";
 
-@IonicPage()
 @Component({
   selector: 'page-set-location',
   templateUrl: 'set-location.html',
 })
 export class SetLocationPage {
+    location: Location;
+    marker: Location;
+    constructor(private navParams: NavParams, private viewCtrl: ViewController) {
+        // this.location = this.navParams.get('location');
+    }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    ionViewWillLoad() {
+        this.location = this.navParams.get('location');
+        if(this.navParams.get('isSet')) {
+            this.marker = this.location;
+        }
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SetLocationPage');
-  }
+    onSetMarker(event: any) {
+        this.location = event.coords;
+        this.marker = new Location(event.coords.lat, event.coords.lng);
+    }
 
+    onConfirm() {
+        this.viewCtrl.dismiss({location: this.marker});
+    }
+    onAbort() {
+        this.viewCtrl.dismiss();
+    }
 }
+
